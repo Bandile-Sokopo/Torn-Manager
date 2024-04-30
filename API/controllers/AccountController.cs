545a71ee0126a8292975b23e0b5b89c3a9d2467e
Bandile-Sokopo <sokopobandile18@gmail.com>
2024-04-30 13:26:43 +0200

    -Added RegisterDTO -Altered accountdto to take input from user

--- API/controllers/AccountController.cs
+++ API/controllers/AccountController.cs
@@ -1,7 +1,9 @@
 ﻿using Api.Controllers;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -17,18 +19,25 @@ namespace API.controllers
         }
 
         [HttpPost("register")]//api/account/register
-        public async Task<ActionResult<User>> Register(string username, string password)
+        public async Task<ActionResult<User>> Register(RegisterDTO register)
         {
+            if(await UserExists(register.Username)) return BadRequest("Username is taken");
+
             using var hmac = new HMACSHA512();
             var user = new User
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = register.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(register.Password)),
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
