681a2c91cb3360722370eb489df4021771ab69ea
Bandile-Sokopo <sokopobandile18@gmail.com>
2024-06-06 16:51:29 +0200

    -Persisting the login -Register and Homepage created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,12 +10,16 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -9,23 +11,28 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent {
 
   model: any = {};
-  loggedIn = false;
+  currentUser$: Observable<User | null> = of(null);
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$;
+  }
 
   login()
   {
     this.accountService.login(this.model).subscribe({
       next: response =>
       {
-        this.loggedIn = true;
+        console.log(response);
       },
       error: error => console.log(error)
     })
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
+    
   }
 
 }
