409739e66bb6ded732fabebe691bb6eaad973d2e
Bandile-Sokopo <sokopobandile18@gmail.com>
2024-06-12 19:26:24 +0200

    -Routing set up

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FactionsComponent } from './factions/factions.component';
+import { NpcLootComponent } from './npc-loot/npc-loot.component';
+import { StocksComponent } from './stocks/stocks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'factions', component: FactionsComponent },
+  { path: 'npc-loot', component: NpcLootComponent },
+  { path: 'stocks', component: StocksComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
