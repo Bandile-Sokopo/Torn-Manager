cf5b7344217582106251bc55a5232c0e98c50932
Bandile-Sokopo <sokopobandile18@gmail.com>
2024-06-20 09:17:23 +0200

    -added routing guards

--- API/Program.cs
+++ API/Program.cs
@@ -23,18 +23,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 builder.Services.AddCors();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { FactionsComponent } from './factions/factions.component';
 import { NpcLootComponent } from './npc-loot/npc-loot.component';
 import { StocksComponent } from './stocks/stocks.component';
+import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'factions', component: FactionsComponent },
+  { path: 'factions', component: FactionsComponent, canActivate: [authGuard] },
   { path: 'npc-loot', component: NpcLootComponent },
   { path: 'stocks', component: StocksComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
