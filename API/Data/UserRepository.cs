c752e30f3eb6702e87c4630e0a5c0235bc8f6b81
Bandile-Sokopo <sokopobandile18@gmail.com>
2024-07-16 11:58:56 +0200

    -Getting started with repositories

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users.SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<User> GetUserIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(User user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<User> GetUserIdAsync(int id);
+        Task<User> GetUserByUsernameAsync(string username);
+    }
+}
