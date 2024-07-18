2a3e6288b215d0e14d60fa99b7f0b8a39e57a7a5
Bandile-Sokopo <sokopobandile18@gmail.com>
2024-07-18 08:33:47 +0200

    -updated user entity

--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -7,6 +7,7 @@
         public string? ApiKey { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public List<TransactionHistory> transactions { get; set; } = new();
 
     }
 }
