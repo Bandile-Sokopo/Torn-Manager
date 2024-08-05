e0e90a2f3861526594c32b9d3928311b6b77a0d8
Bandile-Sokopo <sokopobandile18@gmail.com>
2024-08-05 07:52:51 +0200

    -MemberDto added

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace API.DTOs
+{
+	public class MemberDto
+	{
+		public int Id { get; set; }
+		public string? UserName { get; set; }
+		public string KnownAs { get; set; }
+		public DateTime Created { get; set; } = DateTime.UtcNow;
+		public DateTime LastActive { get; set; }
+		public string Gender { get; set; }
+		public PhotoDto photo { get; set; }
+	}
+}
