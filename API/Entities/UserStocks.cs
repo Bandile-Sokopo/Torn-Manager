61644b48ff75286bef9a01ea9e4ffbd400063ee0
Bandile-Sokopo <sokopobandile18@gmail.com>
2024-09-02 09:46:37 +0200

    -Added structure to add the list of stocks the user has purchased

---
+++ API/Entities/UserStocks.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities
+{
+    public class UserStocks
+    {
+        public int Id { get; set; }
+        public string Stock { get; set; }
+        public int Total { get; set; }
+        public bool isActive { get; set; }
+        public int IncrementsActive { get; set; }
+    }
+}
---
+++ API/controllers/UserStockController.cs
@@ -0,0 +1,20 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.controllers
+{
+    public class UserStockController
+    {
+        private readonly DataContext _context;
+        public UserStockController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult<UserStocks> AddStock([FromBody] UserStocks item) { }
+
+        [HttpPut]
+        public IActionResult<UserStocks> UpdateStock([FromBody] UserStocks item) { }
+    }
+}
